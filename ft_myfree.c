faf0815ca9daa4458fab5bc036a26bbf4f8b9e9d
Lauri Salmi <late.salmi@gmail.com>
2022-05-18 11:56:00 +0300

    Adding myfree

--- ft_myfree.c
+++ ft_myfree.c
@@ -6,7 +6,7 @@
 /*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 12:06:08 by lasalmi           #+#    #+#             */
-/*   Updated: 2022/05/13 15:40:17 by lasalmi          ###   ########.fr       */
+/*   Updated: 2022/05/13 15:40:45 by lasalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_myfree(void *ptr)
 {
 	size_t	*to_free;
 
-	to_free = (size_t *)*ptr;
+	to_free = (size_t *)ptr;
 	to_free = to_free - 2;
 	free(to_free);
 }
